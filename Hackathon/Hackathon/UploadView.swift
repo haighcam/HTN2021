0f22d2db9b35fbced10b09b3524db0a794d60d12
Thomas <shijixing119@gmail.com>
2021-01-16 17:50:53 -0500

    hourly commit

--- Hackathon/Hackathon/UploadView.swift
+++ Hackathon/Hackathon/UploadView.swift
@@ -1,46 +1,46 @@
-////
-////  UploadView.swift
-////  Hackathon
-////
-////  Created by Hanbo Yu on 2021-01-16.
-////
 //
-//import SwiftUI
+//  UploadView.swift
+//  Hackathon
 //
-//struct UploadView: View {
-//    @State var showImagePickerView: Bool = false
-//    @State var image : UIImage? = nil
-//    @State var text : String? = nil
-//    var body: some View {
-//        VStack{
-//            if image != nil{
-//                Image(uiImage: image!)
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .padding(.vertical, CGFloat(10))
-//            }
-//            if text != nil{
-//                Text(text!)
-//            }
-//            Button(action:{
-//                self.showImagePickerView = true
-//            }){
-//                HStack{
-//                    Text("Pick Image")
-//                }
-//            }.foregroundColor(.white)
-//            .background(Color.blue)
-//            .cornerRadius(40)
-//            .padding(10)
-//            .sheet(isPresented: self.$showImagePickerView){
-//                ImagePicker(image: self.$image,text:self.$text)
-//            }
-//        }
-//    }
-//}
+//  Created by Hanbo Yu on 2021-01-16.
 //
-//struct UploadView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        UploadView()
-//    }
-//}
+
+import SwiftUI
+
+struct UploadView: View {
+    @State var showImagePickerView: Bool = false
+    @State var image : UIImage? = nil
+    @State var text : String? = nil
+    var body: some View {
+        VStack{
+            if image != nil{
+                Image(uiImage: image!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.vertical, CGFloat(10))
+            }
+            if text != nil{
+                Text(text!)
+            }
+            Button(action:{
+                self.showImagePickerView = true
+            }){
+                HStack{
+                    Text("Pick Image")
+                }
+            }.foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(40)
+            .padding(10)
+            .sheet(isPresented: self.$showImagePickerView){
+                ImagePicker(image: self.$image,text:self.$text)
+            }
+        }
+    }
+}
+
+struct UploadView_Previews: PreviewProvider {
+    static var previews: some View {
+        UploadView()
+    }
+}
