fe8b925f7d865e401b074d16935b11b13b5ef0e6
Thomas <shijixing119@gmail.com>
2021-01-16 01:06:30 -0500

    Sample swift

---
+++ Hackathon/Hackathon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hackathon
+//
+//  Created by Hanbo Yu on 2021-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hackathon/Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by Hanbo Yu on 2021-01-16.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
