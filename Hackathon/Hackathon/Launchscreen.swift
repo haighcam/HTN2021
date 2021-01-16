d6f6ff019b2fbdb42a3d2c61f85b8d42a67da5b7
Hanbo Yu <Hanbo.Yu@ibm.com>
2021-01-16 11:31:28 -0500

    .

---
+++ Hackathon/Hackathon/Launchscreen.swift
@@ -0,0 +1,20 @@
+//
+//  Launchscreen.swift
+//  Hackathon
+//
+//  Created by Hanbo Yu on 2021-01-16.
+//
+
+import SwiftUI
+
+struct Launchscreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Launchscreen_Previews: PreviewProvider {
+    static var previews: some View {
+        Launchscreen()
+    }
+}
