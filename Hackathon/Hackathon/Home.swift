9ce29b1dccf095b7f6c968847f68f7dda94a9bfe
Hanbo Yu <Hanbo.Yu@ibm.com>
2021-01-16 11:38:41 -0500

    Delete unrelated dir

--- Hackathon/Hackathon/Home.swift
+++ Hackathon/Hackathon/Home.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct Home: View {
     var body: some View {
         VStack(spacing:40){
@@ -94,10 +95,4 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
-
-//struct homebutton_Previews: PreviewProvider {
-//    static var previews: some View {
-//        homebutton()
-//    }
-//}
 
