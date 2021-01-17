48e1f49960f9a3054d2d8cea3c5d12548b21137e
AdityaSaigal2000 <aditya.saigal@mail.utoronto.ca>
2021-01-17 03:11:30 -0500

    Added mock up home page with broken links to gallery and live page. Expected to be fixed once entire app comes together

---
+++ Hackathon/CaptionGen_AndroidUI/app/src/main/java/com/example/captiongen/MainActivity3.java
@@ -0,0 +1,26 @@
+package com.example.captiongen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    public void Gallery(View view){
+        Intent intent = new Intent(MainActivity3.this, gallery.class);
+        startActivity(intent);
+    }
+
+    public void Live(View view){
+        Intent intent = new Intent(MainActivity3.this, live.class);
+        startActivity(intent);
+    }
+}
